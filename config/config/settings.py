9adcaaa7abedb3c3582ae4e439b9122a06198393
kallamvishnupriya <vishnupriyakallam@gmail.com>
2026-02-14 16:14:45 +0530

    done

--- config/config/settings.py
+++ config/config/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'gym',
+    'config.gym',
     'corsheaders',
     'django_filters',
 ]
